c4c2a27ad4b29926ccb957ccd58d45b533e1010f
bmcoles <77514803+bmcoles@users.noreply.github.com>
2021-03-07 13:58:16 -0500

    Update jsonHandler.py

--- jsonHandler.py
+++ jsonHandler.py
@@ -2,41 +2,50 @@
 # It will also read in current json files for already created and in-progress tasks
 # The purpose of this is to track the progress of all tasks and store information for them for later use
 # Authored by Brennan Coles, ADD AS YOU WORK ON IT
-# General TODO - Get program integrated with c shell instead of standing alone
+
+# TODO - Get program integrated with c shell instead of standing alone
+
 import json
 
-# TODO - Configure the options to work with c shell input
-option = 'n'
-option = input ("Enter r for read or w for write: ")
+option = 'n' # Used extensively for the user input
+search = 'n' # Used later for the search feature
 
-while option == 'r' or option == 'w':
+while option !='r' and option !='w': # Loop guarantees only valid input is accepted
+    option = input ("Enter r for read or w for write: ")
+
+while option == 'r' or option == 'w': # Loop runs only while the user specifies to read and write
     if option == 'r':
-        # TODO - Configure the open statement to take data passed in from the c shell
-        f = open('C:\\Users\\hyper\\Documents\\data.json')
-        readData = json.load(f)
-        for i in readData['tasks']:
-            print(i)
-        option = input("Type w to write to a file, r to read another file, or any other value to quit: ")
+        readData =json.load(open('C:\\Users\\hyper\\Documents\\data.json'))
+        while (option != 'a' and option !='s'):
+            option = input("Type a to see all entries or s to search for a specific task: ")
+        if (option == 'a'):
+            while (option !='t' and option !='f' and option !='b'):
+                option = input ("Type t to see just the tasks, f to see just the time frame, and b for both: ")
+            for i in readData:
+                if (option == 't'):
+                    print(i)
+                if (option == 'f'):
+                    print(readData[i])
+                if (option == 'b'):
+                    print(i + " " + readData[i])
+        if (option == 's'):
+            # TODO Implement the search feature
+            print("NOT IMPLEMENTED YET")
+            # while (option != 'c'):
+                 #search = input("Type the task you would like to search for")
 
+        option = input("Type w to write to a file, r to read another file, or any other value to quit: ")
 
     if option == 'w':
         toAdd = 'g';
-        data = {} # Will be named User_data
-        data['tasks'] = []
+        data = {}  # Will be named User_data
         # The purpose of this loop is to take input from the user to set up and create a .json file of their tasks
         while toAdd != 'q':
-            # EXAMPLE
-            # 'Lose 10 pounds':'One week'
             # TODO - Configure input to work with the c shell
             task = input("Enter the task name: ")
-            timeFrame = input("Enter the time frame to complete: ")
-            data['tasks'].append({
-                "task" : task,
-                "timeFrame" : timeFrame
-            })
+            timeFrame = input("Enter the time frame to complete this task: ")
+            data[task] = timeFrame
             toAdd = input("Enter q to quit or any other value to continue: ")
-
-        # Stores the data to an output file
-        with open ('C:\\Users\\hyper\\Documents\\data.json', 'w') as outfile:
+        with open('C:\\Users\\hyper\\Documents\\data.json', 'w') as outfile:
             json.dump(data, outfile)
         option = input("Type w to write to another file, r to read a file, or any other value to quit: ")
